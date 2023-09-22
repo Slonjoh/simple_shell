fe30f2225403c99a49f96fe172be17d118cac9c3
Slonjoh <tobson11122@gmail.com>
2023-09-22 12:20:16 -0700

    testing

--- shell_path.c
+++ shell_path.c
@@ -1,44 +1,6 @@
 #include "tobonyshell.h"
 
 /**
- * find_executable -  function is responsible for searching for the
- * executable file in the PATH and returning the full path if found.
- * @command: a string
- * Return: a copy of the path
- */
-
-
-char *find_executable(char *command)
-{
-	char *path = getenv("PATH");
-
-	if (path)
-	{
-		char *path_copy = strdup(path);
-		char *token = strtok(path_copy, ":");
-
-		while (token)
-		{
-			char full_path[INPUT_LENGTH];
-
-			snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-
-			if (access(full_path, X_OK) == 0)
-			{
-				char *executable;
-
-				executable = strdup(full_path);
-				free(path_copy);
-				return (executable);
-			}
-			token = strtok(NULL, ":");
-		}
-		free(path_copy);
-	}
-	return (NULL);
-}
-
-/**
  * execute_command - a function that handles the actual execution of
  * the command and checks if the executable is found before forking.
  * @executable: string executable
@@ -87,4 +49,42 @@ void execute_command(char *executable, char *args[])
 		exit(exit_status);
 	else
 	exit_status = 0;
+}
+
+/**
+ * find_executable -  function is responsible for searching for the
+ * executable file in the PATH and returning the full path if found.
+ * @command: a string
+ * Return: a copy of the path
+ */
+
+
+char *get_command(char *execute)
+{
+        char *path = getenv("PATH");
+
+        if (path)
+        {
+                char *path_copy = strdup(path);
+                char *token = strtok(path_copy, ":");
+
+                while (token)
+                {
+                        char full_path[INPUT_LENGTH];
+
+                        snprintf(full_path, sizeof(full_path), "%s/%s", token, execute);
+
+                        if (access(full_path, X_OK) == 0)
+                        {
+                                char *executable;
+
+                                executable = strdup(full_path);
+                                free(path_copy);
+                                return (executable);
+                        }
+                        token = strtok(NULL, ":");
+                }
+                free(path_copy);
+        }
+        return (NULL);
 }
